280cf3f5885397a66e0c473f8a919e9a6fc531fa
VeTaL2022 <vetal.kots2003@gmail.com>
2022-10-19 20:24:47 +0300

    React Project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import {Navigate, Route, Routes} from "react-router-dom";
+import useLocalStorage from "use-local-storage";
 
 import {GenresPage, HomePage, MoviesPage} from "./pages";
 import {MainLayout} from "./layouts";
 import './App.css';
 
 function App() {
+    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
 
-  return (
-    <Routes>
-      <Route path={'/'} element={<MainLayout/>}>
-          <Route index element={<Navigate to={'home'}/>}/>
-          <Route path={'home'} element={<HomePage/>}/>
-          <Route path={'movies'} element={<MoviesPage/>}/>
-          <Route path={'genres'} element={<GenresPage/>}/>
-      </Route>
-    </Routes>
-  );
+    const switchTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+    };
+
+
+    return (
+        <div className='App' data-theme={theme}>
+            <button onClick={switchTheme}>
+                Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
+            </button>
+            <Routes>
+                <Route path={'/'} element={<MainLayout/>}>
+                    <Route index element={<Navigate to={'home'}/>}/>
+                    <Route path={'home'} element={<HomePage/>}/>
+                    <Route path={'movies'} element={<MoviesPage/>}/>
+                    <Route path={'genres'} element={<GenresPage/>}/>
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -6,10 +6,10 @@ export const MovieInfo = ({movie}) => {
 
     return (
         <div className={css.Info}>
-            <img src={'https://image.tmdb.org/t/p/w185' + poster_path} alt="img"/>
+            <img src={'https://image.tmdb.org/t/p/w185' + poster_path} alt="img" width={'210px'}/>
             <h3>{title}</h3>
             <p>{overview}</p>
-            <p>{<StarsRating rate={vote_average}/>}</p>
+            <div>{<StarsRating rate={vote_average}/>}</div>
         </div>
     )
 
