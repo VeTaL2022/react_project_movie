78dc88772557f1a0d3c22f345b29879091e5dc4b
VeTaL2022 <vetal.kots2003@gmail.com>
2022-10-20 21:42:47 +0300

    React Project

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -9,16 +9,20 @@ export const HomePage = () => {
         <Carousel className={css.Carousel}>
             <div>
                 <img src="https://sportshub.cbsistatic.com/i/2022/09/29/d3da7fd2-b22f-4fb1-a121-2524d9da3db0/black-adam-imax-poster-art.jpg" alt={"black adam"}/>
-                {/*<p className={'legend'}>Legend 1</p>*/}
+
             </div>
             <div>
                 <img src="https://static.posters.cz/image/1300/%D0%9F%D0%BB%D0%B0%D0%BA%D0%B0%D1%82%D0%B8/
                 shazam-one-sheet-i71939.jpg" alt={"shazam"}/>
-                {/*<p className={'legend'}>Legend 2</p>*/}
             </div>
             <div>
                 <img src="https://images.thedirect.com/media/photos/thor1.jpg" alt={"thor"}/>
-                {/*<p className={'legend'}>Legend 3</p>*/}
+            </div>
+            <div>
+                <img src="https://www.themoviedb.org/t/p/original/7rIPjn5TUK04O25ZkMyHrGNPgLx.jpg" alt="superman"/>
+            </div>
+            <div>
+                <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b70001104204173.5f5e25f4318f6.jpg" alt="iron man"/>
             </div>
         </Carousel>
     )
